f052a175c8ac00d644ca20f891b37a3ecab691ab
Arnab-arch <arnabsheoran801@gmail.com>
2025-12-22 15:53:17 +0530

    initial react

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function Profile(){
+  return(
+    <img 
+    src = "https://i.imgur.com/MK3eW3As.jpg"
+    alt="Katherine Johnson"/>
+
+    
+  );
+}
+export default function (){
+  return(
+    <section>
+      <h1>TOP SCIENTIST</h1>
+      <Profile/>
+      <Profile/>
+      <Profile/>
+    </section>
+  );
+}
